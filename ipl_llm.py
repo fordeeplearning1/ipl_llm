8b3dd368974d38dc8df7acc51095d0615a1115b6
fordeeplearning1 <fordeeplearning1@gmail.com>
2023-04-19 18:53:09 +0530

    added readme and requirements

--- ipl_llm.py
+++ ipl_llm.py
@@ -6,12 +6,10 @@ from langchain.llms import HuggingFacePipeline
 # from langchain.docstore.document import Document
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import AnalyzeDocumentChain
-
 import requests
 from bs4 import BeautifulSoup
 
 # import os
-
 from googlesearch import search
 
 hf = HuggingFacePipeline.from_model_id(model_id='google/flan-t5-xl', task="text2text-generation")
@@ -21,7 +19,9 @@ qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=chain)
 s = search("today's ipl match prediction", num_results=10, advanced=True)
 
 url_ls = [i.url for i in s]
-query = "Which team will win today's match between LSG & RR?"
+# query = "Which team will win today's match between LSG & RR?"
+
+query = input('\n Enter your IPL match question: ')
 
 for url in url_ls:
     print(url)
